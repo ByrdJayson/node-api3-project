1e5bba35904662b8a0f075127caa8d18851dca5e
Jayson Byrd <jayson_byrd_95@outlook.com>
2022-01-12 15:09:50 -0500

    Returns 404 if user not found

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,13 +1,22 @@
+const User = require('../users/users-model')
+
 function logger(req, res, next) {
   const time = new Date().toLocaleString()
   console.log(time, req.method, req.originalUrl, )
   next()
 }
 
-function validateUserId(req, res, next) {
-  console.log('scaffolding log')
-  next()
-  
+async function validateUserId(req, res, next) {
+  try {
+    const user = await User.getById(req.params.id)
+    if(!user){
+      res.status(404).json({message: "User does not exist"})
+    } else {
+      req.user = user
+    }
+  } catch (err) {
+    next({status: 500, message: "Internal Error"})
+  }
 }
 
 function validateUser(req, res, next) {
--- api/server.js
+++ api/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { logger } = require('./middleware/middleware')
 const server = express();
-
+const userRouter = require('./users/users-router')
 server.use(express.json())
 // global middlewares and the user's router need to be connected here
 server.use(logger)
+server.use('./api/users', userRouter)
 
 
 server.get('/', (req, res) => {
